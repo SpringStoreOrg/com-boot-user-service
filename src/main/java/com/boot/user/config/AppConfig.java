f0f82a98576ea40ebdbce105c14da592cb0150e5
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2021-10-10 20:12:23 +0300

    Update AppConfig.java

--- src/main/java/com/boot/user/config/AppConfig.java
+++ src/main/java/com/boot/user/config/AppConfig.java
@@ -13,31 +13,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.ui.velocity.VelocityEngineFactory;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import com.boot.user.client.CartServiceClient;
 import com.boot.user.client.ProductServiceClient;
 
 @Configuration
 public class AppConfig {
-
-	@Bean
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("OPTIONS");
-		config.addAllowedMethod("GET");
-		config.addAllowedMethod("POST");
-		config.addAllowedMethod("PUT");
-		config.addAllowedMethod("DELETE");
-		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
-	}
 
 	@Bean
 	PasswordEncoder passwordEncoder() {
