f0150872175fae50301cee7c0ea88e84ae341d94
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2022-03-29 10:24:12 +0300

    security updated

--- src/main/java/com/boot/user/config/AppConfig.java
+++ src/main/java/com/boot/user/config/AppConfig.java
@@ -3,14 +3,23 @@ package com.boot.user.config;
 import java.io.IOException;
 import java.util.Properties;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.exception.VelocityException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.ui.velocity.VelocityEngineFactory;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,9 +27,14 @@ import com.boot.user.client.CartServiceClient;
 import com.boot.user.client.ProductServiceClient;
 
 @Configuration
-public class AppConfig {
+@EnableWebSecurity
+public class AppConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private UserDetailsService userDetailsService;
 
+	@Autowired
+	private JwtConfig jwtConfig;
 
 	@Bean
 	public RestTemplate template() {
@@ -36,7 +50,7 @@ public class AppConfig {
 	public CartServiceClient cartServiceClient() {
 		return new CartServiceClient();
 	}
-	
+
 	@Bean
 	public JavaMailSender getJavaMailSender() {
 		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
@@ -47,25 +61,69 @@ public class AppConfig {
 		mailSender.setUsername("noreply@springwebstore.com");
 		mailSender.setPassword("QQQwwwEEE1234$");
 
-        Properties javaMailProperties = new Properties();
-        javaMailProperties.put("mail.smtp.starttls.enable", "true");
-        javaMailProperties.put("mail.smtp.auth", "true");
-        javaMailProperties.put("mail.transport.protocol", "smtp");
-        javaMailProperties.put("mail.debug", "true");
- 
-        mailSender.setJavaMailProperties(javaMailProperties);
+		Properties javaMailProperties = new Properties();
+		javaMailProperties.put("mail.smtp.starttls.enable", "true");
+		javaMailProperties.put("mail.smtp.auth", "true");
+		javaMailProperties.put("mail.transport.protocol", "smtp");
+		javaMailProperties.put("mail.debug", "true");
+
+		mailSender.setJavaMailProperties(javaMailProperties);
 
 		return mailSender;
 	}
 
-    @Bean
-    public VelocityEngine getVelocityEngine() throws VelocityException, IOException {
-        VelocityEngineFactory velocityEngineFactory = new VelocityEngineFactory();
-        Properties props = new Properties();
-        props.put("resource.loader", "class");
-        props.put("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
- 
-        velocityEngineFactory.setVelocityProperties(props);
-        return velocityEngineFactory.createVelocityEngine();
-    }
+	@Bean
+	public VelocityEngine getVelocityEngine() throws VelocityException, IOException {
+		VelocityEngineFactory velocityEngineFactory = new VelocityEngineFactory();
+		Properties props = new Properties();
+		props.put("resource.loader", "class");
+		props.put("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+
+		velocityEngineFactory.setVelocityProperties(props);
+		return velocityEngineFactory.createVelocityEngine();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+				// make sure we use stateless session; session won't be used to store user's
+				// state.
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				// handle an authorized attempts
+				.exceptionHandling()
+				.authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)).and()
+				// Add a filter to validate user credentials and add token in the response
+				// header
+				// What's the authenticationManager()?
+				// An object provided by WebSecurityConfigurerAdapter, used to authenticate the
+				// user passing user's credentials
+				// The filter needs this auth manager to authenticate the user.
+				.addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig))
+				.authorizeRequests()
+				// allow all POST requests
+				.antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
+				// any other requests must be authenticated
+				.anyRequest().authenticated();
+	}
+
+	// Spring has UserDetailsService interface, which can be overriden to provide
+	// our implementation for fetching user from database (or any other source).
+	// The UserDetailsService object is used by the auth manager to load the user
+	// from database.
+	// In addition, we need to define the password encoder also. So, auth manager
+	// can compare and verify passwords.
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+
+	@Bean
+	public JwtConfig jwtConfig() {
+		return new JwtConfig();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
