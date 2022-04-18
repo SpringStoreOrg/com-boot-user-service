48f1abe0d96860c57d208bb1ded7d48cad3bc732
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2022-04-18 10:08:01 +0300

    checkTokenAvailability updated

--- src/main/java/com/boot/user/validator/TokenValidator.java
+++ src/main/java/com/boot/user/validator/TokenValidator.java
@@ -1,6 +1,9 @@
 package com.boot.user.validator;
 
 import java.text.ParseException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -9,14 +12,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class TokenValidator {
 
-	public boolean checkTokenAvailability(Date date) throws ParseException {
-		//You could implement this logic using:
-		//LocalDateTime.now().isBefore(Instant.ofEpochMilli(date.getTime())
-                //.atZone(ZoneId.systemDefault())
-                //.toLocalDateTime().plusHours(1))
-		Date now = Calendar.getInstance().getTime(); // Get time now
-		long differenceInMillis = now.getTime() - date.getTime();
-		long differenceInHours = (differenceInMillis) / 1000L / 60L / 60L; // Divide by millis/sec, secs/min, mins/hr
-		return (int) differenceInHours > 1 ? true : false;
-	}
+    public boolean checkTokenAvailability(Date date) {
+
+        return LocalDateTime.now().isBefore(Instant.ofEpochMilli(date.getTime())
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime().plusHours(1));
+    }
 }
