d1444d1123fe9fa16e64fe7bae05d41169f34626
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2022-10-19 17:13:46 +0300

    updated from junit4 to junit5

--- src/test/java/com/boot/user/SpringUserServiceApplicationTests.java
+++ src/test/java/com/boot/user/SpringUserServiceApplicationTests.java
@@ -1,12 +1,10 @@
 package com.boot.user;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringUserServiceApplicationTests.class)
 @EnableWebMvc
 public class SpringUserServiceApplicationTests {
