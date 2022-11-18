fa9409986eb7535f9ac788a1088edb13e40ae92b
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2022-11-18 13:21:55 +0200

    @EnableWebMvc added back to  SpringUserServiceApplicationTests

--- src/test/java/com/boot/user/SpringUserServiceApplicationTests.java
+++ src/test/java/com/boot/user/SpringUserServiceApplicationTests.java
@@ -3,8 +3,10 @@ package com.boot.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootTest(classes = SpringUserServiceApplicationTests.class)
+@EnableWebMvc
 public class SpringUserServiceApplicationTests {
 
 	@Test
