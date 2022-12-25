f96989bfad9dedb681e87b893817041833d2543b
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2022-12-25 15:55:34 +0200

    Update UserController.java

--- src/main/java/com/boot/user/controller/UserController.java
+++ src/main/java/com/boot/user/controller/UserController.java
@@ -7,12 +7,13 @@ import com.boot.user.service.UserService;
 import com.boot.user.util.Constants;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -26,10 +27,10 @@ import java.util.List;
 
 @Validated
 @Controller
+@AllArgsConstructor
 @Tag(name = "user", description = "the User API")
 public class UserController {
 
-    @Autowired
     private UserService userService;
 
     @Operation(summary = "Create a new user", description = "Create a new user with the given information", tags = {"user"})
@@ -41,32 +42,37 @@ public class UserController {
     public ResponseEntity<UserDTO> addUser(@Parameter(description = "User to add. Cannot null or empty.",
             required = true, schema = @Schema(implementation = UserDTO.class))
                                            @Valid @RequestBody UserDTO user) {
-       UserDTO userDTO =  userService.addUser(user);
-        return new ResponseEntity<>(userDTO,HttpStatus.CREATED);
+        UserDTO userDTO = userService.addUser(user);
+        return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
     }
 
     @Operation(summary = "Update an existing user by email", description = "Update an existing user using user's email address", tags = {"user"})
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully updated" , content = @Content(schema = @Schema(implementation = UserDTO.class))),
+            @ApiResponse(responseCode = "200", description = "Successfully updated", content = @Content(schema = @Schema(implementation = UserDTO.class))),
             @ApiResponse(responseCode = "404", description = "User not found", content = @Content)})
     @PutMapping(value = "/{email}", consumes = {"application/json"}, produces = {"application/json"})
     public ResponseEntity<UserDTO> updateUserByEmail(@Parameter(description = "User to be updated.",
             required = true, schema = @Schema(implementation = UserDTO.class)) @Valid @RequestBody UserDTO userDTO,
-                                                     @Parameter(description = "Email of the user to be updated. Cannot be empty.",
-                                                             required = true) @Email(message = "Invalid email!", regexp = Constants.EMAIL_REGEXP) @PathVariable("email") String email) throws EntityNotFoundException {
+                                                     @Email(message = "Invalid email!", regexp = Constants.EMAIL_REGEXP) @PathVariable("email") String email) throws EntityNotFoundException {
         UserDTO user = userService.updateUserByEmail(email, userDTO);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-    @Operation(summary = "Activate user account", description = "", tags = {"user"})
-    @PutMapping(value = "/confirm/{token}")
+    @Operation(summary = "Activate user account", description = "Activate user account using the confirmation token", tags = {"user"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User activated Successfully", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Token not found", content = @Content)})
+    @PutMapping(value = "/confirm/{token}", consumes = {"application/json"})
     public ResponseEntity<String> confirmUserAccount(@PathVariable("token") String token)
             throws EntityNotFoundException, UnableToModifyDataException {
         userService.confirmUserAccount(token);
         return new ResponseEntity<>("User activated Successfully!", HttpStatus.OK);
     }
 
-    @Operation(summary = "Get all users", description = "", tags = {"user"})
+    @Operation(summary = "Get all users", tags = {"user"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Users found successfully",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = UserDTO.class))))})
     @GetMapping("/users")
     @ResponseBody
     public ResponseEntity<List<UserDTO>> getAllUsers() throws EntityNotFoundException {
@@ -74,7 +80,10 @@ public class UserController {
         return new ResponseEntity<>(userList, HttpStatus.OK);
     }
 
-    @Operation(summary = "Get user by email", description = "", tags = {"user"})
+    @Operation(summary = "Get user by email", description = "Get a specific user by email address", tags = {"user"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User found successfully", content = @Content(schema = @Schema(implementation = UserDTO.class))),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content)})
     @GetMapping
     @ResponseBody
     public ResponseEntity<UserDTO> getUserByEmail(@Email(message = "Invalid email!", regexp = Constants.EMAIL_REGEXP) @RequestParam String email) throws EntityNotFoundException {
@@ -82,7 +91,10 @@ public class UserController {
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-    @Operation(summary = "Delete user by email", description = "", tags = {"user"})
+    @Operation(summary = "Delete user by email", description = "Delete a specific user by email address", tags = {"user"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User successfully deleted", content = @Content()),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content)})
     @DeleteMapping("/{email}")
     public ResponseEntity<UserDTO> deleteUserByByEmail(@Email(message = "Invalid email!", regexp = Constants.EMAIL_REGEXP) @PathVariable("email") String email)
             throws EntityNotFoundException {
