7d4a19af3d1d6aceff0ff29d7a0db7692b234f80
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2023-12-10 09:32:15 +0200

    Update CartServiceClient.java

--- src/main/java/com/boot/user/client/CartServiceClient.java
+++ src/main/java/com/boot/user/client/CartServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "cart-service/cart")
+@FeignClient(name = "cart-service")
 public interface CartServiceClient {
 
     @DeleteMapping
